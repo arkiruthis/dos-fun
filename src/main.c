979a15d650501ec0c667dba83e47c5be93cd0161
Nick Anderson <arkiruthis@gmail.com>
2025-11-28 11:31:06 +0000

    Lets be consistent with our commenting style.

--- src/main.c
+++ src/main.c
@@ -23,7 +23,7 @@ int main(void)
   int t = 0;
   time_t current_time;
 
-  /* map physical 0xA0000 into our flat address space */
+  // Map physical 0xA0000 into our flat address space 
 #ifdef __DJGPP__
   __djgpp_nearptr_enable();
   vga = (unsigned char *)(__djgpp_conventional_base + 0xA0000);
@@ -31,16 +31,16 @@ int main(void)
   vga = (unsigned char *)0xA0000;
 #endif
 
-  /* set VGA mode 13h */
+  // Set VGA mode 13h
   set_video_mode(0x13);
 
   current_time = time(NULL);
 
-  /* simple animation loop */
+  // Simple animation loop
   for (t = 0; t < 1000; ++t)
   {
     ptr = &back[0];
-    /* draw into back buffer only */
+    // Draw into back buffer only
     for (i = 0; i < SIZE; ++i)
     {
       x = i;
@@ -48,7 +48,7 @@ int main(void)
       *ptr++ = (unsigned char)((x ^ t) + y);
     }
 
-    /* blit back buffer -> VGA in one go */
+    // Blit back buffer -> VGA in one go
     memcpy(vga, &back[0], SIZE);
   }
   current_time = time(NULL) - current_time;
@@ -58,7 +58,7 @@ int main(void)
 
   getch();
 
-  /* back to text mode 3 */
+  // Back to text mode 3
   set_video_mode(0x03);
 
   return 0;
